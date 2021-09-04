81991456f123867ddf0b86b3704f1d0fea7ef205
YatharthDive <71880750+YatharthDive@users.noreply.github.com>
2021-09-04 14:38:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+leftWristX=0
+leftWristY=0
+rightWristX=0
+rightWristY=0
 image=""
 song=""
 function preload(){
-    song=loadSound("music.mp3")
+    song=loadSound("Money Hiest.mp3")
     
 }
 function setup(){
@@ -9,11 +13,31 @@ function setup(){
     canvas.center();
     v1=createCapture(VIDEO);
     v1.hide()
-    //document.getElementById("songimage1").src=image;
+    posenet=ml5.poseNet(v1,modelLoaded)
+    posenet.on("pose",gotPoses)
+    document.getElementById("songimage1").src=image;
 }
+function gotPoses(results) {
+    if (results.length>0) {
+        console.log(results)
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        rightWristX=results[0].pose.rightWrist.x;
+        righWristY=results[0].pose.rightWrist.y;
+    }
+}
+function modelLoaded() {
+    console.log("modelLoaded")
+}
+
 function draw(){
     image(v1,0,0,600,500)
-}
+}       
 function play(){
     song.play();
+    song.setVolume(1)
+    song.rate(1)
+    image="Money Hiest image.jpg"
+    document.getElementById("songimage1").src=image;
+    document.getElementById("song").innerHTML="Playing Money Heist";
 }
