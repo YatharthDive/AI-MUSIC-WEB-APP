df1a282dbce4a0d7fd247ad1cb5c2324862a98f9
YatharthDive <71880750+YatharthDive@users.noreply.github.com>
2021-09-13 15:01:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ leftWristX = 0
 leftWristY = 0
 rightWristX = 0
 rightWristY = 0
+scoreRW = 0
 image = ""
 song1 = ""
 song2 = ""
@@ -29,6 +30,7 @@ function gotPoses(results) {
         //console.log(results)
         leftWristX = results[0].pose.leftWrist.x;
         scoreLW = results[0].pose.keypoints[9].score;
+        scoreRW = results[0].pose.keypoints[10].score;
         //console.log(scoreLW)
         leftWristY = results[0].pose.leftWrist.y;
         rightWristX = results[0].pose.rightWrist.x;
@@ -57,7 +59,20 @@ function draw() {
             document.getElementById("song").innerHTML = "Playing Money Heist";
         }
     }
+
+    if (scoreRW > 0.2) {
+
+        circle(rightWristX, rightWristY, 20)
+        song1.stop()
+        if (song2_status == false) {
+            song2.play()
+            image = "Harry Potter.jpg"
+            document.getElementById("songimage1").src = image;
+            document.getElementById("song").innerHTML = "Playing Harry Potter";
+        }
+    }
 }
+
 function play() {
     song1.play();
     song1.setVolume(1)
